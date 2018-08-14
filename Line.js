b53871fc555de50d7832ef4d7ce6537fb0f7387d
Angelos Lechouritis <angelos.lexouritis@gmail.com>
2018-08-14 10:44:03 +0300

    fixed PropTypes

--- Line.js
+++ Line.js
@@ -1,61 +1,60 @@
-import React, {
-    Component,
-} from 'react'
-import {
-    StyleSheet,
-    View,
-} from 'react-native'
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import * as Utils from './Utils'
-import {PropTypes} from "prop-types";
+import * as Utils from "./Utils";
+import { PropTypes } from "prop-types";
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-    }
-})
+  container: {
+    position: "absolute"
+  }
+});
+
+Line.propTypes = {
+    color: PropTypes.string.isRequired,
+    lineWidth: PropTypes.number,
+    start: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+    }).isRequired,
+    end: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+    }).isRequired,
+}
 
 export default class Line extends Component {
+  //static defaultProps = {
+  //    lineWidth: 1,
+  //}
 
-    //static defaultProps = {
-    //    lineWidth: 1,
-    //}
+  // 构造
+  constructor(props) {
+    super(props);
+    // 初始状态
+    this.state = {};
+  }
 
-    static propTypes = {
-        color: PropTypes.string.isRequired,
-        lineWidth: PropTypes.number,
-        start: PropTypes.shape({
-            x: PropTypes.number.isRequired,
-            y: PropTypes.number.isRequired,
-        }).isRequired,
-        end: PropTypes.shape({
-            x: PropTypes.number.isRequired,
-            y: PropTypes.number.isRequired,
-        }).isRequired,
-    }
+  render() {
+    let transform = Utils.getLineTransform(this.props.start, this.props.end);
 
-    // 构造
-    constructor (props) {
-        super(props)
-        // 初始状态
-        this.state = {}
-    }
-
-    render () {
-        let transform = Utils.getLineTransform(this.props.start, this.props.end)
-
-        return (
-            <View
-                style={[styles.container, {
-                    backgroundColor: this.props.color,
-                    width: transform.distance,
-                    height: this.props.lineWidth,
-                    left: this.props.start.x,
-                    top: this.props.start.y - this.props.lineWidth / 2,
-                    transform: [{translateX: transform.translateX},
-                        {translateY: transform.translateY},
-                        {rotateZ: transform.rotateRad + 'rad'}]
-                  }]}/>
-        )
-    }
-
+    return (
+      <View
+        style={[
+          styles.container,
+          {
+            backgroundColor: this.props.color,
+            width: transform.distance,
+            height: this.props.lineWidth,
+            left: this.props.start.x,
+            top: this.props.start.y - this.props.lineWidth / 2,
+            transform: [
+              { translateX: transform.translateX },
+              { translateY: transform.translateY },
+              { rotateZ: transform.rotateRad + "rad" }
+            ]
+          }
+        ]}
+      />
+    );
+  }
 }
